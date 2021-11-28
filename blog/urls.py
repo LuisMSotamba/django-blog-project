ca0a68be4b5d208c02dd12986e3e30b5626c7f1f
Luis Miguel Sotamba <luis.sotamba@ucuenca.edu.ec>
2021-11-28 11:12:29 -0500

    Create template blog

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #post_list se utiliza para obtener la vista a renderizar
+    #el namee parameter: nombre de la URL
+    path('',views.post_list,name="post_list")    
+]
