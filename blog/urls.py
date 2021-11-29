88a9f3a72bdb3fc4c5d393ccc95b992d1314ca82
Luis Miguel Sotamba <luis.sotamba@ucuenca.edu.ec>
2021-11-28 23:29:18 -0500

    See specific post

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     #post_list se utiliza para obtener la vista a renderizar
     #el namee parameter: nombre de la URL
-    path('',views.post_list,name="post_list")    
+    path('',views.post_list,name="post_list"), 
+    path('post/<int:pk>/', views.post_detail, name='post_detail') 
 ]
